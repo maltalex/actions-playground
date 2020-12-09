f22a080a502228b5595752f6f22f52994e1c40df
maltalex <code@bit48.net>
2020-12-09 20:57:43 +0200

    passing and failing test

---
+++ src/test/java/com/github/maltalex/actions/TestClass.java
@@ -0,0 +1,16 @@
+package com.github.maltalex.actions;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+
+	@Test
+	public void passing() {
+	}
+
+	@Test
+	public void failing() {
+		fail();
+	}
+}
